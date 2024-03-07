7b81f1c1cdfdad00f640446b42fa85e96fdbdde3
Alex Caicedo <chevyagcr@gmail.com>
2024-03-07 13:08:40 -0500

    Agregar archivos de proyecto.

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -553,5 +553,10 @@ namespace InventarioComputadoras.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
