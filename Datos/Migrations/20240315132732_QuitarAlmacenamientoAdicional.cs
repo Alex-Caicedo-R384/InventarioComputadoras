52e0f7224d2627683a6d4108db2856faca8d130d
Alex Caicedo <chevyagcr@gmail.com>
2024-03-15 08:32:21 -0500

    Igual, hacer lo de discos adicionales

---
+++ Datos/Migrations/20240315132732_QuitarAlmacenamientoAdicional.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventarioComputadoras.Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuitarAlmacenamientoAdicional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlmacenamientosAdicionales");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlmacenamientosAdicionales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComputadoraId = table.Column<int>(type: "int", nullable: false),
+                    Capacidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroParte = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroSerie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlmacenamientosAdicionales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AlmacenamientosAdicionales_Computadoras_ComputadoraId",
+                        column: x => x.ComputadoraId,
+                        principalTable: "Computadoras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlmacenamientosAdicionales_ComputadoraId",
+                table: "AlmacenamientosAdicionales",
+                column: "ComputadoraId");
+        }
+    }
+}
