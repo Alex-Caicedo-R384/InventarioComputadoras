39f5ca190716f611a3a0a28046e3cc6bf9c5daaf
Alex Caicedo <chevyagcr@gmail.com>
2024-03-21 11:10:58 -0500

    Completado

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -481,7 +481,6 @@ namespace InventarioComputadoras.Controllers
                         }
                     }
                 }
-                _contexto.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
 
